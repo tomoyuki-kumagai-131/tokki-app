13965d55e785d041325c4341ec062e69362c6f21
tomoyuki.kumagai <woovoodoor@yahoo.co.jp>
2023-02-19 18:09:18 +0900

    delete: helo

--- src/components/parts/Forums/Hero.stories.tsx
+++
@@ -1,8 +0,0 @@
-import { ComponentMeta } from '@storybook/react'
-
-import { Hero } from './Forums'
-
-export default { component: Hero } as ComponentMeta<typeof Hero>
-export const Default = {
-	args: {},
-}
