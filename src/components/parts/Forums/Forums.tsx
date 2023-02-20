5ba8212317c23bee9be7f1a2f8a2b78261bb4846
tomoyuki.kumagai <woovoodoor@yahoo.co.jp>
2023-02-20 09:15:32 +0900

    fix: grid

--- src/components/parts/Forums/Forums.tsx
+++ src/components/parts/Forums/Forums.tsx
@@ -6,100 +6,96 @@ import {
 	CardFooter,
 	CardHeader,
 	Grid,
+	GridItem,
 	Heading,
-	Image,
 	SimpleGrid,
-	Text,
 } from '@chakra-ui/react'
+import Image from 'next/image'
 import Link from 'next/link'
-// import { Carousel } from 'react-responsive-carousel'
-// Import Swiper React components
-// import { Swiper, SwiperSlide } from 'swiper/react'
 
-import 'swiper/css'
-// import SwiperCore, { Autoplay, Navigation, Pagination } from 'swiper'
-// import { Header } from '~/components/layouts/Layout/Header'
-
-// SwiperCore.use([Navigation, Pagination, Autoplay])
+const MEMBERS = [
+	{
+		name: 'Haerin',
+		image: '/images/haerin.jpeg',
+		href: '/members/haerin',
+	},
+	{
+		name: 'Hannni',
+		image: '/images/hanni.jpeg',
+		href: '/members/hanni',
+	},
+	{
+		name: 'Danielle',
+		image: '/images/danielle.jpeg',
+		href: '/members/danielle',
+	},
+	{
+		name: 'Minji',
+		image: '/images/minji.jpeg',
+		href: '/members/minji',
+	},
+	{
+		name: 'Hyein',
+		image: '/images/hyein.jpeg',
+		href: '/members/hyein',
+	},
+	{
+		name: 'NewJeans',
+		image: '/images/1.jpg',
+		href: '/members/newjeans',
+	},
+]
 
 export const Forums: React.FC = () => {
 	return (
-		<Grid
-			display={'flex'}
-			templateColumns="repeat(2, 1fr)"
-			gap={4}
-			mx={{ base: 4, md: 16 }}
-			mt={4}
-			textAlign="center"
-		>
-			<Card maxW={{ base: 200, md: 800 }} w={{ base: 190, md: 400 }}>
-				<CardHeader>
-					<Heading size="md">Haerin</Heading>
-				</CardHeader>
-				<CardBody display="flex" justifyContent={'center'}>
-					<Image
-						src="/images/haerin.jpeg"
-						h={200}
-						fill="Background"
-						alt={''}
-						borderRadius={4}
-					/>
-				</CardBody>
-				<CardFooter textAlign={'right'}>
-					<Box
-						textAlign={'center'}
-						display="flex"
-						justifyContent={'center'}
-						mx={'auto'}
-					>
-						<Button
-							fontWeight="bold"
-							color={'white'}
-							bg="gray.300"
-							_hover={{
-								bg: 'pink.400',
-								color: 'white',
-							}}
-						>
-							<Link href="/members/haerin">Haerin</Link>
-						</Button>
-					</Box>
-				</CardFooter>
-			</Card>
-			<Card maxW={{ base: 200, md: 800 }} w={{ base: 190, md: 400 }}>
-				<CardHeader>
-					<Heading size="md">Hannni</Heading>
-				</CardHeader>
-				<CardBody display="flex" justifyContent={'center'}>
-					<Image
-						src="/images/hanni.jpeg"
-						h={200}
-						fill="Background"
-						alt={''}
-						borderRadius={4}
-					/>
-				</CardBody>
-				<CardFooter>
-					<Box
-						textAlign={'center'}
-						display="flex"
-						justifyContent={'center'}
-						mx={'auto'}
-					>
-						<Button
-							fontWeight={600}
-							color={'white'}
-							bg="gray.300"
-							_hover={{
-								bg: 'red.400',
-								color: 'white',
-							}}
-						>
-							<Link href="/members/haerin">Hanni</Link>
-						</Button>
-					</Box>
-				</CardFooter>
-			</Card>
-		</Grid>
+		<>
+			<Grid
+				// display={'flex'}
+				justifyContent={'center'}
+				alignItems={'center'}
+				gap={4}
+				mx={{ base: 6, md: 16 }}
+				mt={4}
+				textAlign="center"
+				templateColumns={{ base: 'repeat(2, 1fr)', md: 'repeat(5, 1fr)' }}
+			>
+				{MEMBERS.map((member, index) => (
+					<GridItem key={index}>
+						<Card maxW={{ base: 200, md: 800 }} w={{ base: 165, md: 240 }}>
+							<CardHeader>
+								<Link href={member.href}>
+									<Heading size="md">{member.name}</Heading>
+								</Link>
+							</CardHeader>
+							<CardBody display="flex" justifyContent={'center'}>
+								<Link href={member.href}>
+									<Image src={member.image} height={100} width={150} alt={''} />
+								</Link>
+							</CardBody>
+							<CardFooter textAlign={'right'}>
+								<Box
+									textAlign={'center'}
+									display="flex"
+									justifyContent={'center'}
+									mx={'auto'}
+								>
+									<Button
+										fontWeight="bold"
+										color={'white'}
+										bg="gray.300"
+										_hover={{
+											bg: 'pink.400',
+											color: 'white',
+										}}
+									>
+										<Link href={member.href}>{member.name}</Link>
+									</Button>
+								</Box>
+							</CardFooter>
+						</Card>
+					</GridItem>
+				))}
+			</Grid>
+		</>
 	)
 }
